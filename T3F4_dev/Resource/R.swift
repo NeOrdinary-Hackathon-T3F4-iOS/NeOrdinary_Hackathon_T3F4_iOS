02bb735d4a3b22b17f024f345b91c1d466d1da12
rudqlsdla <hsmikvyv@gmail.com>
2025-05-17 18:15:06 +0900

    feat: R.Color enum 세팅

---
+++ T3F4_dev/Resource/R.swift
@@ -0,0 +1,61 @@
+//
+//  R.swift
+//  T3F4_dev
+//
+//  Created by 임경빈 on 5/17/25.
+//
+
+import Foundation
+
+public enum R {
+  enum Color: String, CaseIterable {
+    case neutral_black = "neutral-black"
+    case neutral_white = "neutral-white"
+    case neutral_10 = "neutral-10"
+    case neutral_20 = "neutral-20"
+    case neutral_30 = "neutral-30"
+    case neutral_40 = "neutral-40"
+    case neutral_50 = "neutral-50"
+    case neutral_60 = "neutral-60"
+    case neutral_70 = "neutral-70"
+    case neutral_80 = "neutral-80"
+    case neutral_90 = "neutral-90"
+    case primary_default = "primary-default"
+    case primary_disabled = "primary-disabled"
+    case primary_hover = "primary-hover"
+    case primary_pressed = "primary-pressed"
+    case primary_subtle = "primary-subtle"
+    case error_default = "error-default"
+    case error_disabled = "error-disabled"
+    case error_hover = "error-hover"
+    case error_pressed = "error-pressed"
+    case info_default = "info-default"
+    case info_disabled = "info-disabled"
+    case info_hover = "info-hover"
+    case info_pressed = "info-pressed"
+    case success_default = "success-default"
+    case success_disabled = "success-disabled"
+    case success_hover = "success-hover"
+    case success_pressed = "success-pressed"
+    case warning_default = "warning-default"
+    case warning_disabled = "warning-disabled"
+    case warning_hover = "warning-hover"
+    case warning_pressed = "warning-pressed"
+    case text_default = "text-default"
+    case text_inverse = "text-inverse"
+    case text_strong = "text-strong"
+    case text_subtle = "text-subtle"
+    case background_default = "background-default"
+    case background_inverse = "background-inverse"
+    case background_strong = "background-strong"
+    case background_subtle = "background-subtle"
+    case border_default = "border-default"
+    case border_inverse = "border-inverse"
+    case border_strong = "border-strong"
+    case border_subtle = "border-subtle"
+    case surface_default = "surface-default"
+    case surface_inverse = "surface-inverse"
+    case surface_strong = "surface-strong"
+    case surface_subtle = "surface-subtle"
+  }
+}
