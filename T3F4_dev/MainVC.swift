67206179ea6a72d14be497fcad6235194a6065b5
Seungwan98 <sinna3210@gmail.com>
2025-05-17 16:31:29 +0900

    add: 폰트, FontManger, 폴더정리 추가

--- T3F4_dev/MainVC.swift
+++ T3F4_dev/MainVC.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
---
+++ T3F4_dev/SplashVC.swift
@@ -0,0 +1,75 @@
+//
+//  SplashVC.swift
+//  T3F4_dev
+//
+//  Created by 양승완 on 5/17/25.
+//
+
+
+import Foundation
+import UIKit
+
+
+/**
+ 스플래시 화면 뷰 컨트롤러
+ */
+class SplashVC: UIViewController {
+    
+    private var maxseconds: Int = 2
+    private var timer = Timer()
+    private var logo = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .clear
+        self.navigationController?.navigationBar.isHidden = true
+        makelogo()
+        runtimer()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func makelogo() {
+        self.view.addSubview(logo)
+        
+        
+        logo.image = UIImage(named: "logo.png")
+        logo.contentMode = .scaleAspectFit
+        
+        
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        //        logo.snp.makeConstraints {
+        //            $0.center.equalToSuperview()
+        //            $0.width.equalTo(266)
+        //            $0.height.equalTo(92)
+        //        }
+        
+        
+    }
+    
+    
+    func runtimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updatetimer), userInfo: nil, repeats: true)
+    }
+    
+    /**
+     타이머 동작 함수
+     */
+    @objc func updatetimer() {
+        maxseconds = maxseconds - 1
+        print(maxseconds)
+        if maxseconds == 0 {
+            timer.invalidate()
+            print("timer Invalidate")
+            navigationController?.viewControllers = [MainVC()]
+            return
+        } else if maxseconds < 0 {
+            timer.invalidate()
+            return
+        }
+        
+    }
+    
+}
